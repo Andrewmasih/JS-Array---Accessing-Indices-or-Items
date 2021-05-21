0f1f99f9399d0d7312d3ee3f550091d14dcf62ce
Andrew Masih <armasih@outlook.com>
2021-05-21 17:51:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let gymMembershipNumbers = [152478, 452658, 758453, 254163, 896247, 521369, 584658, 236514, 785698, 236548];
+
+console.log(gymMembershipNumbers.indexOf(452658));
+console.log(gymMembershipNumbers.indexOf(254163));
+console.log(gymMembershipNumbers.indexOf(521369));
+console.log(gymMembershipNumbers.indexOf(785698));
+
+/* we can also get the index of the item we need by using indexOf then the value inside the parentheses  */
+
+
